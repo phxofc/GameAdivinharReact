8d3c2fa11149bcfc8472bb8f096fe4b76c351d0b
phxofc <phdfrocha99@gmail.com>
2023-12-18 08:59:33 -0300

    tela inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StartScreen from './assets/components/StartScreen'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div>
-       <h2>Adivinhe a Palavra</h2>
+       <StartScreen/>
       </div>
       
     </>
---
+++ src/assets/components/StartScreen.jsx
@@ -0,0 +1,15 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+    <div className='start'>
+
+        <h1>Adivinhe a Palavra</h1>
+        <p>Clique no botão abaixo para jogar!</p>
+        <button>Começar o Game</button>
+
+    </div>
+  )
+}
+
+export default StartScreen
