34c45d96348ae65d0faac9679274ce827c6ed459
phxofc <phdfrocha99@gmail.com>
2023-12-18 08:24:55 -0300

    algumas configurações

---
+++ src/assets/data/words.js
@@ -0,0 +1,7 @@
+export const wordList = {
+    animais: ['Cachorro', 'Gato', 'Pássaro', 'Coelho', 'Hamster', 'Peixe', 'Tartaruga', 'Rato', 'Porco', 'Cavalo'],
+    frutas: ['Maçã', 'Banana', 'Morango', 'Uva', 'Abacaxi', 'Melancia', 'Pêra', 'Laranja', 'Limão', 'Pêssego'],
+    transporte: ['Carro', 'Ônibus', 'Bicicleta', 'Avião', 'Barco', 'Caminhão', 'Moto', 'Trem', 'Helicóptero', 'Trator'],
+    eletronicos: ['Computador', 'Telefone', 'Tablet', 'Mouse', 'Teclado', 'Monitor', 'Impressora', 'Roteador', 'Caixa de Som', 'Câmera'],
+    natureza: ['Montanha', 'Rio', 'Oceano', 'Deserto', 'Floresta', 'Praia', 'Cachoeira', 'Vale', 'Planalto', 'Ilha']
+  };
