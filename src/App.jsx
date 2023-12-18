3856cc724d625bb363001910879fcc18ebc6fdc4
phxofc <phdfrocha99@gmail.com>
2023-12-18 09:12:52 -0300

    adicionando components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//CSS
 import './App.css'
+//react
+
+
+//data
+import { wordList } from './assets/data/words'
+
+//components
 import StartScreen from './assets/components/StartScreen'
+import Game from './assets/components/Game'
+import GameOver from './assets/components/GameOver'
+
+const stages =[
+  {id:1, name: "start"},
+  {id:2, name: "game"},
+  {id:3, name: "end"},
+]
+
 
 function App() {
   const [count, setCount] = useState(0)
+  const [gameStage, setGameStage]= useState(stages[0].name)
+  const [words] = useState(wordList);
+  console.log(words)
 
   return (
     <>
       <div>
-       <StartScreen/>
+      {gameStage == 'start' &&  <StartScreen/>}
+      {gameStage == 'game' &&  <Game/>}
+      {gameStage == 'end' &&  <GameOver/>}
       </div>
       
     </>
